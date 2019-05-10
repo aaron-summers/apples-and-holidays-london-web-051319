3e57acbda902025cb239739c346298f06be63eb8
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:17:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
         new_occasion << x.capitalize!
       end
       occasion = new_occasion.join(" ")
-      items = items.join(" ")
+      items = items.join(", ")
 end
 
 def all_holidays_with_bbq(holiday_hash)
