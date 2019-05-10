b0a1adac090edb2167d4b11da86b35fd89397c47
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:06:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, occasion|
     puts season.to_s.capitalize << ":"
-    occasion.eah do |occasion
+    occasion.each do |occasion, items|
+      puts 
 end
 
 def all_holidays_with_bbq(holiday_hash)
