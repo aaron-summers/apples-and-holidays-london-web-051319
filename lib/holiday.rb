e9c0b7c6cf71b886e4b7723a9d9ff397cd2472ee
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:21:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, occasion|
     puts "#{season.to_s.capitalize!}:"
     occasion.each do |occasion, items|
-      occasion_array = occasion.to_s.split("")
+      occasion_array = occasion.to_s.split("_")
       new_occasion = []
       occasion_array.each do |x|
         new_occasion << x.capitalize!
