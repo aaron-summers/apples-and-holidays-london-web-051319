ea57c86d2056de1730a3d6547f8855376a2c210d
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:08:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, occasion|
-    puts "#{season.to_s.capitalize}:"
+    puts "#{season.to_s.capitalize!}:"
     occasion.each do |occasion, items|
       puts 
 end
