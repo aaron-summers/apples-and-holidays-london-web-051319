909662e8a660edf23412f0db0f4e89c21950822f
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:30:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,8 +87,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   result = []
   
-  holiday_hash.each do |occasion, item|
-    
+  holiday_hash.each do |season, occasion|
+    occasion.each do 
 
 end
 
